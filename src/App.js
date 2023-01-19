990bd4699465bd422ae072e4080c59c1c5669606
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-18 23:29:49 -0600

    updated to use react-router

--- src/App.js
+++ src/App.js
@@ -1,45 +1,21 @@
 import React, { useState, useEffect } from "react";
-import format from "date-fns/format";
+import { Routes, Route } from "react-router";
+import { Navigate } from "react-router";
 
 import "./App.css";
 
-function App() {
-  let [posts, setPosts] = useState(null);
-  useEffect(() => {
-    async function fetchData() {
-      const postsData = await fetch(
-        "https://blog-api-production-c97a.up.railway.app/posts"
-      );
-      const posts = await postsData.json();
-      setPosts(posts);
-    }
-    fetchData();
-  }, []);
+import PostsList from "./components/PostsList";
+import PostDetail from "./components/PostDetail";
+import Layout from "./components/Layout";
 
+function App() {
   return (
-    <div className="App">
-      {posts ? (
-        posts.map((post) => {
-          const formattedDate = format(
-            new Date(post.createdAt),
-            "MMMM Do, yyyy"
-          );
-          return (
-            <div className="post">
-              <div className="post-header">
-                <h2>{post.title}</h2>
-                <p>posted on {formattedDate}</p>
-              </div>
-              <p className="post-text">{post.text}</p>
-            </div>
-          );
-        })
-      ) : (
-        <div>
-          <p>There are no posts to display.</p>
-        </div>
-      )}
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="post/:postid" element={<PostDetail />} />
+        <Route index element={<PostsList />} />
+      </Route>
+    </Routes>
   );
 }
 
