b3170cdcd106aac0d9254501ed6d55dd2734354d
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-19 12:42:12 -0600

    removed unused variables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Routes, Route } from "react-router";
-import { Navigate } from "react-router";
 
 import "./App.css";
 
