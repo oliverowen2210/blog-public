e701bb8cb3648f11f03e046e1f4a30f6fd3a305d
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-20 12:57:47 -0600

    extract comments from commentsData

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -39,6 +39,8 @@ const PostDetail = function () {
         const commentsData = await fetch(
           `${process.env.REACT_APP_BLOG_API_URL}/comments/post/${postID}`
         );
+        const comments = await commentsData.json();
+
         setFormattedDate(formatDate(post.createdAt));
         setPost(post);
         setComments(comments);
